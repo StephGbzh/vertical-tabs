18526de9774092b9ff977de9a31d193bc366896b
StephGbzh <username@users.noreply.github.com>
2014-05-03 17:17:21 +0200

    Remove deprecated/window-utils from test

--- test/test-chrome-mod.js
+++ test/test-chrome-mod.js
@@ -2,7 +2,10 @@
 
 const { Ci, Cc } = require("chrome");
 const { ChromeMod } = require("chrome-mod");
-const { windowIterator } = require('sdk/deprecated/window-utils');
+
+const { windows, isDocumentLoaded } = require('sdk/window/utils');
+const { isGlobalPBSupported } = require('sdk/private-browsing/utils');
+const { isPrivateBrowsingSupported } = require('self');
 
 /* Tests for the ChromeMod APIs */
 
@@ -21,10 +24,26 @@ exports.testChromeMod = function(test) {
     },
 
     onAttach: function(worker) {
+        // getWindows() and windowIterator() are copied from deprecated/window-utils (+their dependencies above)
+        function getWindows() windows(null, { includePrivate: isPrivateBrowsingSupported || isGlobalPBSupported });
+        function windowIterator() {
+          // Bug 752631: We only pass already loaded window in order to avoid
+          // breaking XUL windows DOM. DOM is broken when some JS code try
+          // to access DOM during "uninitialized" state of the related document.
+          let list = getWindows().filter(isDocumentLoaded);
+          for (let i = 0, l = list.length; i < l; i++) {
+            yield list[i];
+          }
+        };
+        
       worker.on("message", function (data) {
         test.assertEqual(data, "bye", "get message from content script");
         // Search for this modified window
+        /*for(let win in utils.windows()) {
+            console.log("WINDOW COUCOU1 "+ win)
+        }*/
         for(let win in windowIterator()) {
+            console.log("WINDOW COUCOU "+ win)
           if (win.document.documentElement.getAttribute("chrome-mod-ok") == "true") {
             test.done();
             return;
